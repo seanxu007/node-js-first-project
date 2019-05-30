edcb0f11d9891934c08dc836636a53fdac082ae9
Sean Xu <sean.xu85@gmail.com>
2019-05-30 22:10:16 +1200

    add a simple MVC structure

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
+// const expressHbs = require('express-handlebars');
+
+const errorController = require('./controllers/error');
 
 const app = express();
 
@@ -25,18 +27,16 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const adminRoute = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoute);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Page Not Found'});
-});
+app.use(errorController.get404);
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -2,26 +2,10 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
-
-const adminData = require('./admin');
+const productController = require('../controllers/product');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    const products = adminData.products;
-    // Pug engine
-    // res.render('shop', {prods: products, docTitle: 'Shop', path: '/'});
-    // Hbs engine or Ejs engine
-    res.render('shop', {
-        prods: products,
-        pageTitle: 'Shop',
-        path: '/',
-        hasProducts: products.length > 0,
-        activeShop: true,
-        productCSS: true
-    });
-
-});
+router.get('/', productController.getProducts);
 
 module.exports = router;
